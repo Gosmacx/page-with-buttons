01497472ccfb779a11f10e3d9cd90093f91af371
Gosmac <50182711+Gosmacx@users.noreply.github.com>
2021-07-01 13:09:40 +0300

    🔥🔥

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const ayarlar = {
-    token: "",
+    token: "Botun Tokeni",
     prefix: "!"
 }
 const Discord = require("discord.js")
@@ -34,13 +34,22 @@ client.on('message', message => {
         let embed = new Discord.MessageEmbed()
         .setTitle("Sayfalı Embed")
         .setDescription("Sayfa içeriği \n\n> **Gosmac**")
-        .setFooter("Sayfa 1 / 3")
+        .setFooter("Sayfa 1 / 3")//Sayfa eklemek isterseniz burdan arttırmanız gerek.
 		message.channel.send({embed, buttons:[sol, sağ]});
 	}
 });
 
+
+
+const sayfalar = [//Sayfaların içeriklerini burdan düzenleyebilirsiniz.
+    "Sayfa içeriği \n\n> **Gosmac**",
+    ":cat: Kedileri Severim \n\n> **Gosmac**",
+    ":watermelon: Karpuz Güzeldir! \n\n> **Gosmac**"
+]
+
 client.on('clickButton', async button => {
 
+    //Butonları burdan düzenleyebilirsiniz.
     let sağ = new btn.MessageButton()
     .setID("sağ")
     .setLabel(">>")
@@ -50,31 +59,34 @@ client.on('clickButton', async button => {
     .setLabel("<<")
     .setStyle("blurple");
 
+
 if (button.id === "sağ") {
 
-    let sayfa = button.message.embeds[0].footer.text.split(" ")
+    let sayfa = button.message.embeds[0].footer.text.split(" ")//Embed üzerinden sayfa değerini alıyoruz.
     sayfa[1]++
     if (sayfa[1] == 4) {
         sayfa[1] = 1
     }
-    button.message.embeds[0].footer.text = `Sayfa ${sayfa[1]} / 3`
+    button.message.embeds[0].footer.text = `Sayfa ${sayfa[1]} / 3`//Eğer sayfa eklediyseniz burdaki rakamı arttırmanız gerek.
 
+    //Sayfa parametrelerinin işlevlerini belirliyoruz.
     if (sayfa[1] == 1) {
-        button.message.embeds[0].description = "Sayfa içeriği \n\n> **Gosmac**"
+        button.message.embeds[0].description = sayfalar[0]
     }else if (sayfa[1] == 2) {
-        button.message.embeds[0].description = ":cat: Kedileri Severim \n\n> **Gosmac**"
+        button.message.embeds[0].description = sayfalar[1]
     } else if (sayfa[1] == 3) {
-        button.message.embeds[0].description = ":watermelon: Karpuz Güzeldir! \n\n> **Gosmac**"
+        button.message.embeds[0].description = sayfalar[2]
     }
 
-    button.message.edit({embed: button.message.embeds[0], buttons:[sol, sağ]})
+    button.message.edit({embed: button.message.embeds[0], buttons:[sol, sağ]})//Son olarak embedi editliyoruz.
     button.defer();
 }
 
-if (button.id === "sol") {
 
+if (button.id === "sol") {
+    
     let sayfa = button.message.embeds[0].footer.text.split(" ")
-    sayfa[1]--
+    sayfa[1]-- //Yukarıdaki; sağ butondan tek farkı sayfa parametresini azaltıyoruz.
     if (sayfa[1] == 0) {
         sayfa[1] = 3
     }
@@ -82,11 +94,11 @@ if (button.id === "sol") {
     button.message.embeds[0].footer.text = `Sayfa ${sayfa[1]} / 3`
     
     if (sayfa[1] == 1) {
-        button.message.embeds[0].description = "Sayfa içeriği \n\n> **Gosmac**"
+        button.message.embeds[0].description = sayfalar[0]
     }else if (sayfa[1] == 2) {
-        button.message.embeds[0].description = ":cat: Kedileri Severim \n\n> **Gosmac**"
+        button.message.embeds[0].description = sayfalar[1]
     } else if (sayfa[1] == 3) {
-        button.message.embeds[0].description = ":watermelon: Karpuz Güzeldir! \n\n> **Gosmac**"
+        button.message.embeds[0].description = sayfalar[2]
     }
 
     button.message.edit({embed: button.message.embeds[0], buttons:[sol, sağ]})
