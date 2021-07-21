1c9fe870588f4974264ae8288a4e91acadd8d2ab
Gosmac <50182711+Gosmacx@users.noreply.github.com>
2021-07-22 00:49:14 +0300

    🔥🔥

--- index.js
+++ index.js
@@ -22,13 +22,12 @@ const sayfalar = [//Sayfaların içeriklerini burdan ekleyebilir veya düzenleye
 
 
 client.on('message', message => {
-	if (!message.content.startsWith(ayarlar.prefix) || message.author.bot) return; // Mesaj içeriği öne ek ile başlamazsa geri dön.
 
+	if (!message.content.startsWith(ayarlar.prefix) || message.author.bot) return; // Mesaj içeriği öne ek ile başlamazsa geri dön.
 	const args = message.content.slice(ayarlar.prefix.length).trim().split(/ +/);// Parametler.
 	const command = args.shift().toLowerCase();// Komut
 
 	if (command === 'sayfa') {
-
         let sağ = new btn.MessageButton()
         .setID("sağ")
         .setLabel(">>")
@@ -44,50 +43,34 @@ client.on('message', message => {
         .setFooter(`Sayfa 1 / ${sayfalar.length}`)
 		message.channel.send({embed, buttons:[sol, sağ]});
 	}
+
 });
 
 client.on('clickButton', async button => {
 
-    //Butonları burdan düzenleyebilirsiniz.
-    let sağ = new btn.MessageButton()
-    .setID("sağ")
-    .setLabel(">>")
-    .setStyle("blurple");
-    let sol = new btn.MessageButton()
-    .setID("sol")
-    .setLabel("<<")
-    .setStyle("blurple");
-
-
 if (button.id === "sağ") {
-
     let sayfa = button.message.embeds[0].footer.text.split(" ")//Embed üzerinden sayfa değerini alıyoruz.
     sayfa[1]++
     if (sayfa[1] == sayfalar.length+1) {
         sayfa[1] = 1
     }
-
     button.message.embeds[0].footer.text = `Sayfa ${sayfa[1]} / ${sayfalar.length}`//Embedin footerini yeniliyoruz.
     button.message.embeds[0].description = sayfalar[sayfa[1]-1]//Embedin description kısmını yeniliyoruz.
-
-    button.message.edit({embed: button.message.embeds[0], buttons:[sol, sağ]})//Son olarak embedi editliyoruz.
-    button.defer();
+    button.message.edit({embed: button.message.embeds[0]})//Son olarak embedi editliyoruz.
+    await button.reply.defer();
 }
 
 
 if (button.id === "sol") {
-    
     let sayfa = button.message.embeds[0].footer.text.split(" ")
     sayfa[1]-- //Yukarıdaki "sağ" butondan tek farkı sayfa parametresini azaltıyoruz.
     if (sayfa[1] == 0) {
         sayfa[1] = sayfalar.length
     }
-
     button.message.embeds[0].footer.text = `Sayfa ${sayfa[1]} / ${sayfalar.length}`
     button.message.embeds[0].description = sayfalar[sayfa[1]-1]
-
-    button.message.edit({embed: button.message.embeds[0], buttons:[sol, sağ]})
-    button.defer();
+    button.message.edit({embed: button.message.embeds[0]})
+    await button.reply.defer();
 }
 
 })
